83f647ec524509f3005e34e2d1db185f6f6705f1
anyunseo <anyunseo25@gm.gist.ac.kr>
2025-07-04 21:51:31 +0900

    시쇼ischecked를id가내가선택한id가맞는지비교하고맞으면true아니면false로냅둔다라는것까지는생각했는데그이상이안됩니다!!!!!!!!!!!!!!

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import {useState} from 'react'
+import uuid from 'react-uuid'
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -7,9 +8,8 @@ export const Route = createFileRoute('/')({
 
 export default function Index() {
   
-  const [Input, setInput] = useState('');
-  const [items, setItems] = useState<{id : number, name : string; }[]>([]);
-  const [id, setId] = useState(0);
+  const [Input, setInput] = useState<string>('');
+  const [items, setItems] = useState<{id : string; name : string; isChecked: boolean;}[]>([]);
   
     const handleChange = (event) => {
         setInput(event.target.value);
@@ -17,24 +17,25 @@ export default function Index() {
     };
     const handleSubmit = (event) => {
     event.preventDefault();
-    setItems([...items, {id: id, name: Input}]);
+    setItems(items => [...items, {id: uuid(), name: Input, isChecked: false}]);
     setInput('');
-    setId(id + 1);
     };
+
+    const handleButton = (event) => {
+        event.preventDefault();
+    };
+
+
   
   return (<form onSubmit={handleSubmit}>
     <input type="text" value={Input} onChange={handleChange} />
     <button type="submit" className="bg-[#fff3f3]"> 추가 </button>
     <br />
     <br />
-    <ul>
+    <ul> 
       {items.map((item) =>
-    <li key={item.id}> {item.name} <button className="bg-[#fff3f3]" onClick={() => { setItems(
-                items.filter(a =>
-                  a.id !== item.id
-                )
-              );}
-            }>삭제</button> </li>)}
+    <li key={uuid()} className={`font-semibold 
+     ${item.isChecked ? "line-through text-[#444444]" : "text-[#00AAFF]"}`} > {item.name} <button className="bg-[#fff3f3]" onClick={handleButton}>완료</button> </li>)}
     </ul>
     <br />
     
