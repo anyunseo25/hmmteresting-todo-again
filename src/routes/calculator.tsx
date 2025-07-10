9a7a0fbb9cd47a0b8ee6e8966405bc942f7b7a77
anyunseo <anyunseo25@gm.gist.ac.kr>
2025-07-11 02:09:17 +0900

    일단기능만넣어놨습니다시쇼근데생각보다복잡해졌습니다엉엉

--- src/routes/calculator.tsx
+++ src/routes/calculator.tsx
@@ -9,67 +9,85 @@ export const Route = createFileRoute('/calculator')({
 export default function Calculator() {
   
   const [Input, setInput] = useState<number | string>('');
-  const [numbers, setNumbers] = useState<{id: string; num: number;}[]>([]);
   
+  const [Input2, setInput2] = useState<number | string>('');
+  
+  const [result, setResult] = useState<number>(0);
+  
+  const [click, setClick] = useState<string>('');
+
     const handleChange = (event) => {
         setInput(event.target.value);
       
     };
-    const handleSubmit = (event) => {
-    event.preventDefault();
-    setNumbers(numbers =>  [...numbers, {id: uuid(), num: Input as number}]);
-    setInput('');
+    
+    const handleChange2 = (event) => {
+        setInput2(event.target.value);
+      
     };
 
-    const handleplus = (num: number) => {
-      const plus = Number(Input) + Number(num);
-      setInput(plus);
+    const handleSubmit = (event) => {
+    event.preventDefault();
+  
+    if (click === '+') {
+      setResult(handleplus());
+    } else if (click === '-') {
+      setResult(handleminus());
+    }
+    else if (click === '*') {
+      setResult(handlemultiple());
+    } else if (click === '/') {
+      setResult(handledivide());
+    }
+    setInput('');
+    setInput2('');
+    };
+
+    const handleplus = () => {
+  return  Number(Input2) + Number(Input);
   };
-  const handleminus = (num: number) => {
-    const minus = - Number(Input) + Number(num);
-    setInput(minus);
+  const handleminus = () => {
+  return  Number(Input) - Number(Input2);
 
   };
-  const handlemultiple = (num: number) => {const multiple = Number(Input) * Number(num);
-      setInput(multiple);
+  const handlemultiple = () => {
+  return  Number(Input) * Number(Input2);
   };
-  const handledivide = (num: number) => {
-      const divide =  Number(num) / Number(Input);
-      setInput(divide);
+  const handledivide = () => {
+  return  Number(Input) / Number(Input2);
   };
 
-    const handleButton = (id: string) => {
-          setNumbers(numbers => numbers.filter((n => n.id !== id)));
-        };
+  
+    const clickplus = () => {
+    setClick('+');
+  };
+  const clickminus = () => {
+    setClick('-');
+
+  };
+  const clickmultiple = () => {
+    setClick('*');
+  };
+  const clickdivide = () => {
+    setClick('/');
+  };
 
 
   
   return (<form onSubmit={handleSubmit} className='bg-[#f3f3f3] h-[100vh]'>
     
-    <button className="absolute bg-[#000000] text-[30px] text-[#ffffff] font-[semibold] left-[600px] top-[31.5px]">. - .</button>
+    <input type="text" value={Input} onChange={handleChange}/>
     
-    <button className="absolute bg-[#000000] text-[30px] text-[#ffffff] font-[semibold] left-[680px] top-[31.5px]">. * .</button>
-    
-    <button className="absolute bg-[#000000] text-[30px] text-[#ffffff] font-[semibold] left-[760px] top-[31.5px]">. / .</button>
-
-    <button  className="absolute bg-[#000000] text-[30px] text-[#ffffff] font-[semibold] left-[520px] top-[31.5px]">. + .</button>
-    
-    <input type="text" value={Input} onChange={handleChange} className="text-[30px] w-[500px] h-[60px]" />
-   {numbers.map((number) => <div key = {number.id}><button onClick={() => {handleplus(number.num)}} className="absolute bg-[#000000] text-[30px] text-[#ffffff] font-[semibold] left-[520px] top-[31.5px]">. + .</button>
-    
-    <button onClick={() => {handleminus(number.num)}} className="absolute bg-[#000000] text-[30px] text-[#ffffff] font-[semibold] left-[600px] top-[31.5px]">. - .</button>
-    
-    <button onClick={() => {handlemultiple(number.num)}} className="absolute bg-[#000000] text-[30px] text-[#ffffff] font-[semibold] left-[680px] top-[31.5px]">. * .</button>
-    
-    <button onClick={() => {handledivide(number.num)}} className="absolute bg-[#000000] text-[30px] text-[#ffffff] font-[semibold] left-[760px] top-[31.5px]">. / .</button>
-    </div>)}
+    <input type="text" value={Input2} onChange={handleChange2}/>
+    <button type="button" className="w-10 p-10" onClick={clickplus}>+</button>
+    <button type="button" className="w-10 p-10" onClick={clickminus}>-</button>
+    <button type="button" className = "w-10 p-10" onClick={clickmultiple}>*</button>
+    <button type="button" className="w-10 p-10" onClick={clickdivide}>/</button>
+    <br />
+    <button type="submit">=</button>
     <br />
     <br />
-    <ul> 
-      {numbers.map((number) =>
-    <li key={number.id} > {number.num} <button className="font-semibold text-[20px] bg-[#000000] text-[#ffffff]" onClick={() => handleButton(number.id)}>삭제</button> </li>)}
-    </ul>
-    <br />
+    <div>{result}</div>
     
     
     </form>
